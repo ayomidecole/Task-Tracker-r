8ea6afb110eb4ba31ef5a0f6f2656391eb946a3a
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2025-08-28 04:07:04 -0400

    Refactor JavaScript code for task addition by standardizing quotes and fixing spacing issues.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const addBtn = document.getElementById("add-task");
+const addBtn = document.getElementById('add-task');
 const input = document.getElementById('new-task');
-const taskList = document.querySelector('#task-list ul')
+const taskList = document.querySelector('#task-list ul');
 
 addBtn.addEventListener('click', () => {
-    const taskText = input.value.trim();
-    if (taskText === '') return;
+  const taskText = input.value.trim();
+  if (taskText === '') return;
 
-    const li = document.createElement('li');
-    li.textContent = taskText;
-    taskList.appendChild(li)
+  const li = document.createElement('li');
+  li.textContent = taskText;
+  taskList.appendChild(li);
 
-    input.value = '';
+  input.value = '';
 });
